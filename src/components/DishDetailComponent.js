cc6ce3cd5419978c88b0479f57997e5d20843a21
Abdul-mutaal <abdulmutaalazhar@gmail.com>
2020-05-28 21:28:07 +0500

    Presentational and container components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
-import { render } from '@testing-library/react';
+import { Card, CardImg, CardText, CardBody,CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     renderDish(dish){
         if(dish != null){
             return(
                 <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
@@ -30,13 +26,15 @@ class DishDetail extends Component {
         if(dish) {
             const comm = dish.comments.map((comm) => {
                 return(
-                    <div>
+                    <div key={comm.id}>
                         <li>
-                            <div className='mb-3'>
-                                {comm.comment}
-                            </div>
-                            <div className='mb-3'>
-                                --{comm.author}, {new Date(comm.date).toLocaleDateString()}
+                            <div className="container">
+                                <div className="mb-3">
+                                    {comm.comment}
+                                </div>
+                                <div className="mb-3">
+                                    --{comm.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short',day:'2-digit'}).format(new Date(Date.parse(comm.date)))}
+                                </div>
                             </div>
                         </li>
                     </div>
